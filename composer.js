452199ead818805ccaeba651badc6dc42f8c0578
SelectPdf <office@selectpdf.com>
2017-10-18 14:17:22 +0300

    Create composer.js

---
+++ composer.js
@@ -0,0 +1,23 @@
+{
+    "name": "selectpdf/selectpdf",
+    "type": "library",
+    "version": "2.0.0",
+    "description": "PHP Client library for the SelectPdf REST API. It lets you convert HTML to PDF",
+    "keywords": ["html", "pdf", "html to pdf", "web service", "api", "client", "convert", "converter", "web page"],
+    "homepage": "https://selectpdf.com/html-to-pdf-api/",
+    "license": "MIT",
+    "authors": [
+        {
+            "name": "SelectPdf",
+            "email": "support@selectpdf.com",
+            "homepage": "https://selectpdf.com/html-to-pdf-api/"
+        }
+    ],
+    "require": {
+        "php": ">=5.5",
+        "ext-curl": "*"
+    },
+    "autoload": {
+        "classmap": ["selectpdf.php"]
+    }
+}
